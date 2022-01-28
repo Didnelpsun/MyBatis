8e24806f5e6d4919d6f6f9b38dd5915c01fbb4db
Didnelpsun <2675350965@qq.com>
2022-01-28 14:55:58 +0800

    更新

---
+++ demo3_crud_by_proxy/src/main/java/org/didnelpsun/entity/Query.java
@@ -0,0 +1,13 @@
+package org.didnelpsun.entity;
+
+public class Query {
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
